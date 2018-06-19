2051d68b1092a30db4dd94ecb220f21f63e94a01
Te-Chi Liu <liuderchi@gmail.com>
2018-06-19 23:56:45 +0800

    build(root): introduce precommit hook to clear cell
    - npm init -y && npm i -D glob husky lint-staged
    - add clearCellOutput.js
    - add `precommit`, `clearCellOutput` script

---
+++ clearCellOutput.js
@@ -0,0 +1,34 @@
+#!/usr/bin/env node
+
+const fs = require('fs');
+const glob = require('glob');
+
+const clearCellOutput = content => {
+  const res = JSON.parse(content.slice());
+  res.cells.forEach(cell => {
+    if (cell.outputs && cell.outputs.length > 0) {
+      cell.outputs = [];
+    }
+    if ('execution_count' in cell && cell.execution_count !== null) {
+      cell.execution_count = null;
+    }
+  });
+  return JSON.stringify(res, null, 1) + '\n';
+};
+
+glob('**/*.ipynb', null, function(err, files) {
+  if (err) {
+    throw err;
+  }
+  try {
+    files.forEach(file => {
+      process.stdout.write(`cleaning up ${file} ... `);
+      const contentString = fs.readFileSync(file, 'utf8');
+      const resultString = clearCellOutput(contentString);
+      fs.writeFileSync(file, resultString, 'utf8');
+      process.stdout.write('done');
+    });
+  } catch (e) {
+    throw e;
+  }
+});
