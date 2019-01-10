6c9e7de033975cc2558e1d5c28bf9e8fd966b8b2
liuderchi <liuderchi@gmail.com>
2019-01-10 16:39:57 +0800

    fix build.sh

--- utils/merge-teach-branches.sh
+++ utils/merge-teach-branches.sh
@@ -4,4 +4,4 @@ set -e
 
 git merge --no-ff \
   -m "merge notebooks at $(date +'%m%d/%I-%M%p')" \
-  $(git branch | grep 'teach/' | cut -d, -f2- | tr '\n' ' ' | sed 's/,$//')
+  $(git branch | grep '  teach/' | cut -d, -f2- | tr '\n' ' ' | sed 's/,$//')
